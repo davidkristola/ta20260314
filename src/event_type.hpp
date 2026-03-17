e8b57aaafe689f0b61486cbb0659e349376ce58c
David Kristola <david95037@yahoo.com>
2026-03-16 19:48:06 -0700

    Add optional event information
    Add optional event information and remove VS Code files.

--- src/event_type.hpp
+++ src/event_type.hpp
@@ -1,6 +1,11 @@
 #pragma once
 
+#include <optional>
+
 namespace ta {
+
+using AircraftId = unsigned long int;
+using ChargerId  = unsigned long int;
 
 using HoursType = double;
 
@@ -10,12 +15,18 @@ class EventType {
     int m_id         = 0;
     HoursType m_when = 0.0;
     Cause m_what;
+    std::optional<AircraftId> m_who;
+    std::optional<ChargerId> m_where;
 
 public:
-    EventType(int id, Cause reason, HoursType time) : m_id(id), m_when(time), m_what(reason) {}
+    EventType(int id, Cause reason, HoursType time) noexcept : m_id(id), m_when(time), m_what(reason) {}
     HoursType time() const noexcept { return m_when; }
     int id() const noexcept { return m_id; }
     Cause cause() const noexcept { return m_what; }
+    void set_aircraft(AircraftId id) noexcept { m_who = id; }
+    void set_charger(ChargerId id) noexcept { m_where = id; }
+    std::optional<AircraftId> aircraft() const noexcept { return m_who; }
+    std::optional<ChargerId> charger() const noexcept { return m_where; }
 };
 
 } // namespace ta
--- tdd/tests/test_event_type.cpp
+++ tdd/tests/test_event_type.cpp
@@ -2,5 +2,22 @@
 
 #include "gtest/gtest.h"
 
-TEST(EventType, standard_use_case) {
+TEST(EventType, aircraft) {
+    ta::EventType uut{1, ta::Cause::land, 13.0};
+    const auto null_id = uut.aircraft();
+    EXPECT_FALSE(null_id);
+    uut.set_aircraft(13U);
+    const auto real_id = uut.aircraft();
+    EXPECT_TRUE(real_id);
+    EXPECT_EQ(real_id.value_or(20U), 13U);
+}
+
+TEST(EventType, charger) {
+    ta::EventType uut{1, ta::Cause::start_charging, 13.0};
+    const auto null_id = uut.charger();
+    EXPECT_FALSE(null_id);
+    uut.set_charger(13U);
+    const auto real_id = uut.charger();
+    EXPECT_TRUE(real_id);
+    EXPECT_EQ(real_id.value_or(20U), 13U);
 }
